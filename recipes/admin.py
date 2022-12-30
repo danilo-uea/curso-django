9eada2eeb1da13bdb89643f66f88b011a60e8baf
danilo-uea <dsf.eng17@uea.edu.br>
2022-12-29 20:11:36 -0400

    Recipes no admin e a imagem

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import Category
+from .models import Category, Recipe
 
 
 class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
     pass
 
 
