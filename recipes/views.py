0ae4ac294d692709608ebb4f22ea8c7cf9b5435c
danilo-uea <dsf.eng17@uea.edu.br>
2022-12-31 11:18:29 -0400

    Categoria título e autor nulo

--- recipes/views.py
+++ recipes/views.py
@@ -4,7 +4,7 @@ from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.all().order_by('-id')
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
         # 'recipes': [make_recipe() for _ in range(10)],
@@ -13,9 +13,10 @@ def home(request):
 
 def category(request, category_id):
     recipes = Recipe.objects.filter(
-        category__id=category_id
+        category__id=category_id,
+        is_published=True,
     ).order_by('-id')
-    return render(request, 'recipes/pages/home.html', context={
+    return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
     })
 
