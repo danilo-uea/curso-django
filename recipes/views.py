ebce9ac570d0343db824d88d9623db5ca5b8d1c7
danilo-uea <dsf.eng17@uea.edu.br>
2023-01-02 12:08:41 -0400

    Aula 67 - Melhorando a Home

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,15 @@
-# from django.http import HttpResponse
-from django.http import Http404
-from django.shortcuts import get_list_or_404, render
+from django.shortcuts import get_list_or_404, get_object_or_404, render
 
 from recipes.models import Recipe
 
 
 def home(request):
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True,
+    ).order_by('-id')
+
     return render(request, 'recipes/pages/home.html', context={
         'recipes': recipes,
-        # 'recipes': [make_recipe() for _ in range(10)],
     })
 
 
@@ -28,7 +28,9 @@ def category(request, category_id):
 
 
 def recipe(request, id):
+    recipe = get_object_or_404(Recipe, pk=id, is_published=True,)
+
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': Recipe.objects.get(id=id),
+        'recipe': recipe,
         'is_detail_page': True,
     })
